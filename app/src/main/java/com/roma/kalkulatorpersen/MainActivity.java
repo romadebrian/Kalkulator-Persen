2bece435c538c36198bd7b8d51161f4c616fc3db
romadebrian <roma_coll04@yahoo.com>
2022-01-10 19:24:46 +0700

    persen 2 version no double result

--- app/src/main/java/com/roma/kalkulatorpersen/MainActivity.java
+++ app/src/main/java/com/roma/kalkulatorpersen/MainActivity.java
@@ -12,8 +12,8 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     View Form1;
-    EditText Jumlah, Persen, Hasil;
-    Button Hitung, Mode1, Mode2;
+    EditText Jumlah, Persen, Hasil, Jumlah2, Berapa, Hasil2;
+    Button Hitung, Hitung2, Mode1, Mode2;
 
 
     @Override
@@ -30,31 +30,62 @@ public class MainActivity extends AppCompatActivity {
         Hasil = findViewById(R.id.Frm_Input_Hasil);
         Hitung = findViewById(R.id.Frm_Btn_Hitung);
 
+        Jumlah2 = findViewById(R.id.Frm_Input_Jumlah2);
+        Berapa  = findViewById(R.id.Frm_Input_Berapa);
+        Hasil2  = findViewById(R.id.Frm_Input_Hasil2);
+        Hitung2 = findViewById(R.id.Frm_Btn_Hitung2);
 
-        Hitung.setOnClickListener(new View.OnClickListener() {
+
+
+//        Hitung.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                String input1 = Jumlah.getText().toString();
+//                String input2 = Persen.getText().toString();
+//
+//                if (input1.length() == 0) {
+//                    Toast.makeText(MainActivity.this, "Jumlah Tidak Boleh Kosong", Toast.LENGTH_SHORT).show();
+//                    return;
+//                }
+//                else if (input2.length() == 0) {
+//                    Toast.makeText(MainActivity.this, "Persen Tidak Boleh Kosong", Toast.LENGTH_SHORT).show();
+//                    return;
+//                }
+//                else {
+//                    int jumlah  = Integer.parseInt(input1);
+//                    int persen  = Integer.parseInt(input2);
+//
+//                    int hasil = jumlah * persen / 100;
+//
+//                    Hasil.setText(String.valueOf(hasil));
+//                    Log.d( "Hasilnya: ", String.valueOf(hasil));
+//                }
+//
+//            }
+//        });
+
+        Hitung2.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                String input1 = Jumlah.getText().toString();
-                String input2 = Persen.getText().toString();
+            public void onClick(View v) {
+                String input1 = Jumlah2.getText().toString();
+                String input2 = Berapa.getText().toString();
 
-                if (input1.length() == 0) {
-                    Toast.makeText(MainActivity.this, "Jumlah Tidak Boleh Kosong", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                else if (input2.length() == 0) {
-                    Toast.makeText(MainActivity.this, "Persen Tidak Boleh Kosong", Toast.LENGTH_SHORT).show();
-                    return;
+                if (input1.length() == 0 && input2.length() == 0) {
+                    Toast.makeText(MainActivity.this, "Tidak Boleh Kosong", Toast.LENGTH_SHORT).show();
                 }
                 else {
-                    int jumlah  = Integer.parseInt(input1);
-                    int persen  = Integer.parseInt(input2);
+                    double jumlah   = Integer.parseInt(input1);
+                    double berapa   = Integer.parseInt(input2);
 
-                    int hasil = jumlah * persen / 100;
+                    double hasil    = jumlah / berapa * 100 ;
 
-                    Hasil.setText(String.valueOf(hasil));
+                    int convert  = (int) hasil;
+
+                    String output   = convert + "%";
+
+                    Hasil2.setText(String.valueOf(output));
                     Log.d( "Hasilnya: ", String.valueOf(hasil));
                 }
-
             }
         });
 
